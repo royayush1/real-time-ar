af6bf24678e6f06f6078924fc5a1cbd17205f59c
Ayush Roy <ayushroy@Ayushs-MacBook-Air.local>
2025-02-16 23:16:44 -0700

    ammo selector menu

---
+++ src/app/components/AmmoSelector.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import React from "react";
+
+interface AmmoOption {
+    id: number;
+    label: string;
+    imageSrc: string;
+    damage: number;
+}
+
+interface AmmoSelectorProps {
+    ammoOptions: AmmoOption[]
+    onSelect: (option: AmmoOption) => void;
+}
+
+export default function AmmoSelector({ammoOptions, onSelect}: AmmoSelectorProps){
+    return (
+        <div className="bg-black bg-opacity-70 p-4 rounded">
+            <h3 className="text-white font-bold mb-2">Select Ammo</h3>
+            <div className="flex space-x-2">
+                {ammoOptions.map((option) => (
+                    <button
+                        key={option.id}
+                        onClick={() => (onSelect(option))}
+                        className="flex flex-col items-center hover:scale-105 transform transition-all duration-200">
+                            <img
+                                src={option.imageSrc}
+                                alt={option.label}
+                                className="w-12 h-12 rounded-full object-cover border-2 border-white"/>
+                            <span className="text-white text-xs mt-1">{option.label}</span>
+
+                    </button>
+                ))}
+
+            </div>
+
+        </div>
+    )
+}
